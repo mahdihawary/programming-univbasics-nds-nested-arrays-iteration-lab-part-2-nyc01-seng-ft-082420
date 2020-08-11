66aa484fd17fdfd8c22d23718adef7a20b3a2278
Mahdi Hawary <mahdihawary@gmail.com>
2020-08-11 16:24:02 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ min_values = []
 row_index = 0
 while row_index < src.count do
   element_index = 0
-  lowest_val = 100
+  
   while element_index <src[row_index].count do
     if src[]row_index[element_index] <
 end
